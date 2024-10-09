6ced1f24ad3889a59d0d41c3dbd0e7641ac1a2a1
tuilatin <167632727+tuilatin@users.noreply.github.com>
2024-10-09 10:43:45 +0700

    Add files via upload

---
+++ Tuan6/HinhChuNhat.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thuchanhtuan6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class HinhChuNhat {
+    public Scanner sc=new Scanner(System.in);
+    private int chieuDai;
+    private int chieuRong;
+    public HinhChuNhat(int chieuDai, int chieuRong){
+        this.chieuDai=chieuDai;
+        this.chieuRong=chieuRong;
+    }
+    public int chuVi(){
+        return (this.chieuDai+this.chieuRong)*2;
+    }
+    public int dienTich(){
+        return this.chieuDai*this.chieuRong;
+    }
+    
+    public void inHCN(){
+        System.out.println("Chieu dai hinh chu :"+this.chieuDai);
+        System.out.println("Chieu rong hinh chu nhat :"+this.chieuRong);
+        System.out.println("Chu vi hinh chu nhat la: "+this.chuVi());
+        System.out.println("Dien tich hinh chu nhat la: "+this.dienTich());
+    }
+    public static void main(String[] args) {
+        HinhChuNhat hcn=new HinhChuNhat(5,2);
+        hcn.inHCN();
+    }
+}
